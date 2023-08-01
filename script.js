eeef5a5e46a23a8bf859cc2e524f4fd5a4206491
MikaelaMonroy <mika_monroy00@hotmail.com>
2023-08-01 02:16:39 -0300

    Mi portafolio casi listo

---
+++ script.js
@@ -0,0 +1,9 @@
+function responsiveMenu(){
+    var x= document.getElementById("nav");
+    if(x.className===""){
+        x.className ="responsive";
+
+    }else{
+        x.className="";
+    }
+}
